f4b8f114454da0c996c1c3125ad51cd24a896fe4
Nick Evans <evans2nick@gmail.com>
2017-06-13 21:26:04 -0400

    boiler plate updated

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,14 @@ const gulp = require('gulp');
 const babel = require('babelify');
 const browserify = require('browserify');
 const source = require('vinyl-source-stream');
+const sass = require('gulp-sass');
+const concat = require('gulp-concat');
 const buffer = require('vinyl-buffer');
 const browserSync = require('browser-sync');
 const reload = browserSync.reload;
 const notify = require('gulp-notify');
 const plumber = require('gulp-plumber');
+const historyApiFallback = require('connect-history-api-fallback');
 
 gulp.task('js', () => {
     browserify('src/app.js')
@@ -28,12 +31,23 @@ gulp.task('bs', () => {
     browserSync.init({
         server: {
             baseDir: './'
-        }
+        }, 
+        middleware: [historyApiFallback()]
     });
 });
 
+gulp.task("styles", () => {
+   return gulp.src("./src/styles/**/*.scss")
+   .pipe(sass().on("error",sass.logError))
+   .pipe(concat("style.css"))
+   .pipe(plumber())
+   .pipe(gulp.dest("./public/styles"))
+   .pipe(reload({stream: true}));
+});
 
-gulp.task('default', ['js','bs'], () => {
+
+gulp.task('default', ['js','bs', 'styles'], () => {
     gulp.watch('src/**/*.js',['js']);
     gulp.watch('./public/style.css',reload);
+    gulp.watch('./src/styles/*.scss', ['styles']);
 });
--- src/app.js
+++ src/app.js
@@ -1,2 +1,20 @@
-console.log('it works');
-console.log('niggaaa');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Router, Route, browserHistory, Link } from 'react-router';
+
+class App extends React.Component {
+	render() {
+		return (
+			<div>
+				<h1>Nigga we made it</h1>
+				<p>nigga</p>
+			</div>
+			)
+	}
+}
+
+ReactDOM.render(<Router history={browserHistory}>
+		<Route path="/" component={App}>
+			<Route path="/help" component={App}/>
+		</Route>
+	</Router>, document.getElementById('app'))
